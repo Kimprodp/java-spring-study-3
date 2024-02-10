551238917d1d36e241a3253091ebc4a07e9dbfb9
Gyutae Kim <kiimwf0819@gmail.com>
2024-02-10 16:29:06 +0900

    프로젝트 생성

---
+++ src/main/java/springstudy/thirdprject/ThirdprjectApplication.java
@@ -0,0 +1,13 @@
+package springstudy.thirdprject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdprjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdprjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springstudy/thirdprject/ThirdprjectApplicationTests.java
@@ -0,0 +1,13 @@
+package springstudy.thirdprject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdprjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
